03300ae1565a0b4fb0b7ca943cdea7785d725763
sanderluts <sander.luts@khk.ee>
2019-05-07 10:51:37 +0300

    täiendused kasutaja lisamise skripti

--- praks2/lisa_kasutaja.sh
+++ praks2/lisa_kasutaja.sh
@@ -1,3 +1,11 @@
 #!/bin/bash
-sudo useradd -r -m -s /bin/bash $1
+sudo useradd -r -m -s /bin/bash $1 
 sudo passwd -d $1
+if [ $? -eq 0 ]; then #kontrollib kas väljund puudub, kui väljund puudub, siis kasutaja on lisatud
+	echo "kasutaja $1 on lisatud süsteemi"
+	cut /etc/passwd | grep $1
+	ls -la /home/$1
+else
+	echo "probleem kasutaja lisamisega"
+	echo $? #väljastab errori
+fi
