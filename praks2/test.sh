06ef8fc27b9bc8211098e747aabe27be7c452afc
SanderLuts <37287467+SanderLuts@users.noreply.github.com>
2019-05-16 10:23:11 +0000

    Delete test.sh

--- praks2/test.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-file="/home/user/rsha/praks2/kasutajad.paroolid.txt" #defineerib mis failist lugeda
-while IFS=":" read -r line kasutaja parool #loeb üks rida korraga
-do #loop, mis käivitub iga rea kohta
-echo  $kasutaja
-echo  $parool
-done <"$file"
